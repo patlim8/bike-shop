55be49b11734b06f77953286b6655d269b75ae16
patlim8 <patsap8@gmail.com>
2021-01-08 14:41:03 +0700

    1/8/2021

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ButtonBar from '../components/buttonBar';
+import { connectToDatabase } from "../util/mongodb";
+
 
 export default function Home() {
   return (
