07d0b4f338f1114dff67da29f875dd839c2e5082
Faijiuy <faijiuy44rina@gmail.com>
2021-01-15 00:14:05 +0700

    1/14/21_with_update_delete

--- pages/update_balance.js
+++ pages/update_balance.js
@@ -16,7 +16,7 @@ export default function Home({ balance }) {
     console.log(data)
 
     fetch('/api/balance', {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      method: 'PUT', // *GET, POST, PUT, DELETE, etc.
       mode: 'cors', // no-cors, *cors, same-origin
       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
       credentials: 'same-origin', // include, *same-origin, omit
